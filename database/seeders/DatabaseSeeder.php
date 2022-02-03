aebf319723888f81b3bd5ee942bde57c15f9175a
tezz0386 <dangaura.tejendra.123@gmail.com>
2022-02-03 11:38:14 +0545

    seeder created and noe it full setup to banner for further use

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name'=>'Developer Developer',
+            'email'=>'developer@gmail.com',
+            'password'=>Hash::make('developer'),
+            'role'=>'admin',
+        ]);
+        DB::table('settings')->insert([
+            'name'=>'App Name',
+            'p_email'=>'info@developer.com',
+            'm_no'=>'091982545',
+        ]);
     }
 }
